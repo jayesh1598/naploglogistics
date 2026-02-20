a4abea054e09b2b1c9ef13702ef1c9dd424e73bc
jayesh suthar <39787261+jayesh1598@users.noreply.github.com>
2026-02-20 16:55:31 +0530

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
             <div className="flex items-center gap-3 mb-4">
               {/* ✅ FIXED LOGO */}
               <img
-                src="/images/logo.png"
+                src="assets/c8fae6d8b72b096580232544c5f16dc08c1c047a.png"
                 alt="Naplog Logistics"
                 className="h-12 w-12 object-contain"
               />
